71c46800055f640d1c043941dc4c706b8f97f63d
Шустов Тима <tima_shustov005@mail.ru>
2023-09-21 22:09:50 +0500

    Task0-2

---
+++ Tyuiu.ShustovTS.Sprint1.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShustovTS.Sprint1.Task2.V7.Lib;
+
+namespace Tyuiu.ShustovTS.Sprint1.Task2.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 5; 
+            double res = ds.CalculateSquareCircle(x);
+            Assert.AreEqual(25, res);
+        }
+    }
+}
---
+++ Tyuiu.ShustovTS.Sprint1.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShustovTS.Sprint1.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShustovTS.Sprint1.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fdbdec7f-2c2a-4943-8138-65d8bd60c70b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
