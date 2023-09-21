c12d48df513d8c7b27a53f9b3bb696934abbd32c
Шустов Тима <tima_shustov005@mail.ru>
2023-09-21 23:52:22 +0500

    Task0-2

--- Tyuiu.ShustovTS.Sprint1.Task0.V0/Program.cs
+++ Tyuiu.ShustovTS.Sprint1.Task0.V0/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.ShustovTS.Sprint1.Task0.V13.Lib
+using Tyuiu.ShustovTS.Sprint1.Task0.V13.Lib;
 
 namespace Tyuiu.ShustovTS.Sprint1.Task0.V13
 {
